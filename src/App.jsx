618ff37ad64eef26a5b9a8d281e4a52a4d03bb4b
Suvigya Basnotra <suvigyabasnotra@gmail.com>
2023-07-28 03:11:30 +0530

    Grid responsiveness added

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className="flex flex-col items-center justify-center py-8 gap-4">
         <h1 className="text-xl font-mono mb-2 font-semibold ">ToDo Board</h1>
         <Input taskList={taskList} setTaskList={setTaskList} />
-        <div className="flex flex-col gap-4 sm:grid-cols-3 px-4 sm:px-8 md:px-10">
+        <div className="flex flex-row flex-wrap gap-4 sm:grid-cols-3 px-4 sm:px-8 md:px-10">
           {taskList.map((task, index) => (
             <Board key={index} index={index} task={task} taskList={taskList} setTaskList={setTaskList}/>
           ))}
